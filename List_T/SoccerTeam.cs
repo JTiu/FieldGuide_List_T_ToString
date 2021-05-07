ba0ac403b034740a94922090d6b2091f52ca45e1
James Tiu <jamestiulawyer@gmail.com>
2021-05-07 15:21:36 -0400

    Deleted unused class

--- List_T/SoccerTeam.cs
+++ List_T/SoccerTeam.cs
@@ -17,33 +17,7 @@ namespace List_T
         private int losses = 0;
         private int draws = 0;
 
-        //Property that takes private fields and computes win ratio
-        public double WinRatio
-        {
-            get
-            {
-                int matches = this.wins + this.losses + this.draws;
-                return (double)this.wins / matches;
-            }
-        }
-
-
-        //Method for adding matches to SoccerTeam
-        public void AddResult(int goalsFor, int goalsAgainst)
-        {
-            if (goalsFor > goalsAgainst)
-            {
-                this.wins++;
-            }
-            else if (goalsFor == goalsAgainst)
-            {
-                this.draws++;
-            }
-            else
-            {
-                this.losses++;
-            }
-        }
+       
 
         //Override ToString() class so we can get a nice visual of what our class contains
         public override string ToString()
@@ -51,12 +25,5 @@ namespace List_T
             return this.name + " plays at " + this.stadium + ": " + "W" + this.wins + " L" + this.losses + " D" + this.draws;
         }
 
-        //Constructor which defines how the class is initialized
-        //Takes two arguments (name and stadium)
-        public SoccerTeam(string n, string s)
-        {
-            this.name = n;
-            this.stadium = s;
-        }
     }
 }
