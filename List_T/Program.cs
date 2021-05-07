528d3d3b6603e861ee359d5764e36422220005ef
James Tiu <jamestiulawyer@gmail.com>
2021-05-07 18:42:09 -0400

    Stack and "to string"

--- List_T/Program.cs
+++ List_T/Program.cs
@@ -1,18 +1,41 @@
-﻿using System;
+﻿// C# program to demonstrate
+// Stack ToString() method
+using System;
+using System.Collections;
 
-namespace Lawyers
+class Lawyer
 {
-    public class Example
+
+    public static void Main(String[] args)
     {
-        public static void Main()
+        // Creating an Empty Stack
+        Stack jd = new Stack();
+        Console.WriteLine("The Stack is empty here");
+        Console.ReadLine();
+
+        // Use Push() method
+        // to add elements to 
+        // the stack
+        Console.WriteLine("Pushing to a stack: *jd*, here");
+        jd.Push("PD1\n");
+        jd.Push("PD2");
+        jd.Push("PD3");
+        jd.Push("PD4");
+        jd.Push("PD5");
+        Console.ReadLine();
+        Console.WriteLine("*jd* is a " + jd.GetType());
+        Console.ReadLine();
+
+        Console.WriteLine("Let's apply the 'ToString Method': \n");
+
+        foreach (string str in jd)
         {
-            Prosecutors pros1 = new Prosecutors();
-            PublicDefender pd1 = new PublicDefender();
-            Console.WriteLine(pros1.ToString());
-            Console.WriteLine(pd1.ToString());
-            Console.ReadLine();
+            // Using ToString() method
+            Console.WriteLine(str.ToString());
         }
+
+       
+        Console.ReadLine();
     }
+    
 }
-
-
